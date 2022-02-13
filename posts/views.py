3c954ba6c69a53742108cda1ae4d27bfd1be3856
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-13 02:20:49 +0200

    move paginate method to helpers

--- posts/views.py
+++ posts/views.py
@@ -3,29 +3,15 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.cache import cache
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import (HttpResponse, HttpResponseForbidden,
                          HttpResponseRedirect, JsonResponse)
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
+from utils.helpers import paginate
 
 from . import forms
 from .models import Comment, Post
-
-
-def paginate(objects, page, objects_per_page):
-    '''function take Model Objects, page_number, 
-    elements per page and return paginator object'''
-    paginator = Paginator(objects, objects_per_page)
-    try:
-        paginated_objects = paginator.page(page)
-    except PageNotAnInteger:
-        paginated_objects = paginator.page(1)
-    except EmptyPage:
-        paginated_objects = paginator.page(paginator.num_pages)
-
-    return paginated_objects
 
 
 def home(request):
