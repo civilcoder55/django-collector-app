7fae41bc13bf089be5f833a0c3d05a98af061a07
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-13 02:20:25 +0200

    add helpers.py to contain helper methods

---
+++ utils/helpers.py
@@ -0,0 +1,15 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+
+def paginate(objects, page, objects_per_page):
+    '''function take Model Objects, page_number, 
+    elements per page and return paginator object'''
+    paginator = Paginator(objects, objects_per_page)
+    try:
+        paginated_objects = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_objects = paginator.page(1)
+    except EmptyPage:
+        paginated_objects = paginator.page(paginator.num_pages)
+
+    return paginated_objects
