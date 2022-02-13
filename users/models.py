4988ad659f6ce7537155c4d60a0f9141c87b235c
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-13 09:20:35 +0200

    rename user profile pic to avatar

--- users/models.py
+++ users/models.py
@@ -5,18 +5,18 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='profile_pic')
-    pic = models.ImageField(
-        default='profile_pics/default.jpg', upload_to='profile_pics')
+        User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(
+        default='avatars/default.jpg', upload_to='avatars')
 
     def __str__(self):
         return self.user.username
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.pic.path)
+        img = Image.open(self.avatar.path)
 
         if img.height > 100 or img.width > 100:
             output_size = (250, 250)
             img.thumbnail(output_size)
-            img.save(self.pic.path)
+            img.save(self.avatar.path)
