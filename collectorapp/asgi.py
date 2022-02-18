36a9f7b8a6e2d0815a8d098d240e27c8c491f675
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-18 16:52:15 +0200

    rename project to collector app

--- collectorapp/asgi.py
+++ collectorapp/asgi.py
@@ -3,14 +3,14 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import project.routing
+import collectorapp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collectorapp.settings')
 
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(URLRouter(
-        project.routing.websocket_urlpatterns
+        collectorapp.routing.websocket_urlpatterns
     ))
 })
