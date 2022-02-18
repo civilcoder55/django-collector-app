4138563b14af65777094ad570e8400d74dda9adc
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-18 18:43:22 +0200

    fix typo

--- src/collectorapp/config.py
+++ src/collectorapp/config.py
@@ -16,13 +16,12 @@ REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT')
 REDIS_URI = os.environ.get('REDIS_URI')
 
-
 # MYSQL DATABASE
-DB_NAME = os.environ.get('DB_NAME'),
-DB_USERNAME = os.environ.get('DB_USERNAME'),
-DB_PASSWORD = os.environ.get('DB_PASSWORD'),
-DB_HOST = os.environ.get('DB_HOST'),
-DB_PORT = os.environ.get('DB_PORT'),
+DB_NAME = os.environ.get('DB_NAME')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
 
 # TWITTER SECRETS
 TWITTER_SECRETS = {
