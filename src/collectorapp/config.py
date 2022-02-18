85b026f1b8facf180e1a614994da5c0849df465b
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-18 17:43:03 +0200

    update config and drop loadenv

--- src/collectorapp/config.py
+++ src/collectorapp/config.py
@@ -1,20 +1,38 @@
 import os
 
-APP_ENV = os.getenv("APP_ENV")
-SECRET_KEY = os.getenv("APP_SECRET_KEY")
-APP_URL = os.getenv("APP_URL")
+# DJANGO APPLICATION CONFIGS VARIABLES
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SECRET_KEY = os.environ.get("APP_SECRET_KEY")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
-S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
-S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
+# CUSTOM DJANGO APPLICATION CONFIGS VARIABLES
+APP_ENV = os.environ.get("APP_ENV")
+APP_URL = os.environ.get("APP_URL")
+
+
+# REDIS
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_URI = os.environ.get('REDIS_URI')
+
+
+# MYSQL DATABASE
+DB_NAME = os.environ.get('DB_NAME'),
+DB_USERNAME = os.environ.get('DB_USERNAME'),
+DB_PASSWORD = os.environ.get('DB_PASSWORD'),
+DB_HOST = os.environ.get('DB_HOST'),
+DB_PORT = os.environ.get('DB_PORT'),
+
+# TWITTER SECRETS
 TWITTER_SECRETS = {
-    "consumer_key": os.getenv("TWITTER_CONSUMER_KEY"),
-    "consumer_secret": os.getenv("TWITTER_CONSUMER_SECRET"),
-    "access_token": os.getenv("TWITTER_ACCESS_TOKEN"),
-    "access_token_secret": os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+    "consumer_key": os.environ.get("TWITTER_CONSUMER_KEY"),
+    "consumer_secret": os.environ.get("TWITTER_CONSUMER_SECRET"),
+    "access_token": os.environ.get("TWITTER_ACCESS_TOKEN"),
+    "access_token_secret": os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 }
 
+# DJANGO SOCIAL AUTH CONFIGS
 SOCIAL_AUTH_TWITTER_KEY = TWITTER_SECRETS['consumer_key']
 SOCIAL_AUTH_TWITTER_SECRET = TWITTER_SECRETS['consumer_secret']
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/home/'
