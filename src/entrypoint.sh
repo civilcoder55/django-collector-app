0afec52489c1e8f3ab45a5e3f9f7421d5d50e30c
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-19 19:43:51 +0200

    enhance entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,4 +2,17 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-gunicorn collectorapp.wsgi:application --bind 0.0.0.0:8000
+
+if [ "$APP_ENV" = "dev" ]
+then
+    python manage.py runserver 0.0.0.0:8000
+    # daphne -b 0.0.0.0 -p 8000 collectorapp.asgi:application
+
+elif [ "$APP_ENV" = "worker" ]
+then
+    celery -A collectorapp worker -c 1 & python manage.py runscript collect
+
+else
+    # gunicorn collectorapp.wsgi:application --bind 0.0.0.0:8000
+    daphne -b 0.0.0.0 -p 8000 collectorapp.asgi:application
+fi
