a7fd20703845445bf60340be1f9e1a237507878e
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-20 18:41:36 +0200

    fix worker container

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,14 +3,16 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 
-if [ "$APP_ENV" = "dev" ]
+
+
+if [ "$WORKER" ]
+then
+    celery -A collectorapp worker -c 1 & python manage.py runscript collect
+
+elif [ "$APP_ENV" = "dev" ]
 then
     python manage.py runserver 0.0.0.0:8000
     # daphne -b 0.0.0.0 -p 8000 collectorapp.asgi:application
-
-elif [ "$APP_ENV" = "worker" ]
-then
-    celery -A collectorapp worker -c 1 & python manage.py runscript collect
 
 else
     # gunicorn collectorapp.wsgi:application --bind 0.0.0.0:8000
