e6f8848f0ef18c973faf2fc1feb92d219ee76039
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-22 12:12:08 +0200

    alter post table fileds length

--- src/posts/models.py
+++ src/posts/models.py
@@ -6,14 +6,14 @@ from django.utils import timezone
 class Post(models.Model):
     """Posts [twitter threads] model"""
     id = models.BigIntegerField(primary_key=True)
-    username = models.ManyToManyField(User, default=1)
-    title = models.CharField(max_length=250, default='title')
+    username = models.ManyToManyField(User, blank=True)
+    title = models.CharField(max_length=280, default='title')
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     author_photo = models.CharField(max_length=150)
     author_screen_name = models.CharField(max_length=64)
     author_name = models.CharField(max_length=64)
-    author_describtion = models.CharField(max_length=250)
+    author_describtion = models.CharField(max_length=280)
     rtl = models.BooleanField(default=True)
     thumnail_photo = models.CharField(max_length=150)
     likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
