c534f6673d1d23ee98045d0bd5a0c5bb4da8634e
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-03-04 15:29:42 +0200

    make twitter bot listener tracks dynamic

--- src/scripts/start-stream.py
+++ src/scripts/start-stream.py
@@ -1,7 +1,11 @@
 from lib.CollectorBot.listener import StreamListener
 from utils.logger import logger
+from django.conf import settings
+
 
 def run():
     logger.info('Tweepy stream started')
     stream_listener = StreamListener()
-    stream_listener.filter(track=['@_collectorapp_'], stall_warnings=True)
+    stream_listener.filter(
+        track=[settings.TWITTER_LISTENER_TRACK],
+        stall_warnings=True)
